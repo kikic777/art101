9ff0cb7976fa3a7ebe1b710f63322e727485f310
kikic777 <166079146+kikic777@users.noreply.github.com>
2024-04-23 22:48:08 -0700

    Added js example

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - How to Code Flappy Bird
+// Author: Cynthia Ramirez
+// Date: April 23, 2024
 
 // Constants
 
@@ -19,3 +19,14 @@ function main() {
 
 // let's get this party started
 main();
+// 1. Print start screen<br>
+// 2. Allow user input<br>
+// 3. The user clicks a button to start the game<br>
+// 4a. Mouse click makes the bird fly through obstacles<br>
+  // 4b. The bird falls by itself when nothing happen<br>
+  // 4c. Clicking makes the bird fly upward<br>
+// 5. Randomize the pathway in each game<br>
+// 6. Count the number of sucessful passes<br>
+// 7a. If the bird crashes you lose the game!<br>
+  // 7b. Print game over screen<br>
+// 8. Record data of high score for the user<br>
