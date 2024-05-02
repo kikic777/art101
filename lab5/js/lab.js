c44adb92038aaab3efa8435db4da4746472d5631
kikic777 <166079146+kikic777@users.noreply.github.com>
2024-05-02 00:45:13 -0700

    Edited text

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5: Data Types
+// Author: Cynthia Ramirez
+// Date: April 30, 2024
 
-// Constants
+// Variables
+const car = {};
+car.make = "Volkswagen";
+car.model = "Beetle";
+car.color = "Dark Green";
+car.year = 1969;
+car.age = 2024 - car.year;
 
-// Functions
+// Output
+document.writeln("Make: " + car.make + "<br>")
+document.writeln("Model: " + car.model + "<br>")
+document.writeln("Color: " + car.color + "<br>")
+document.writeln("Year: " + car.year + "<br>")
+document.writeln("Age: " + car.age + "<br>")
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
