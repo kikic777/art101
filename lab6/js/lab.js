f4387a585b025bf1da3b458fca2bb1825f4a2444
kikic777 <166079146+kikic777@users.noreply.github.com>
2024-05-06 17:05:35 -0700

    Editing JS file to have an output object

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Working with a partner, experiment with JavaScript arrays and objects!
+// Author: Cynthia Ramirez
+// Date: 4/29/2024
+
+//Variables
+let myTransport = ["Volkswagen Beetle", "yellow bike", "roller skates", "bus", "rides from family", "black scooter"]
+
+// Objects for my Main Ride
+let myMainRide = {
+  make: "Volkswagen",
+  model: "Beetle",
+  color: "Dark Green",
+  year: 1969,
+  // we can efine a function within our object (called a method) that uses the value of year above (using this.year)
+  age: function() {
+  return 2024 - this.year;
+  }
+};
+
+//Output
+document.writeln("Transportation I Use: " + myTransport + "<br>");
+//This trick allows us to write an object in the document
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
 
 // Constants
 
