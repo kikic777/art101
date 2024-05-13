a325e0c0fa9b3f15879f964dc961cb5d8a873e6f
kikic777 <166079146+kikic777@users.noreply.github.com>
2024-05-13 01:09:19 -0700

    Editing js file

---
+++ lab8/js/lab.js
@@ -0,0 +1,25 @@
+// index.js - Working with a partner, write functions that take user input!
+// Author: Cynthia Ramirez
+// Date: 5/13/2024
+
+function isEven(x){
+    return (x % 2 == 0);
+}
+
+// test function
+console.log("Is 1 even? ", isEven(1));
+console.log("Is 2 even? ", isEven(2));
+
+array = [22, 3, 15, 94, 777, 522, 86, 48]
+// should return (8) [22, 3, 15, 94, 777, 522, 86, 48]
+console.log("My Array: ", array);
+
+let result = array.map(isEven)
+// should return (8) [true, false, false, true, false, true, true, true]
+comsole.log("Eveness of Array: ", result);
+
+array.map(function(x){
+    return x ** 0.5;
+})
+// should return (8) [11, 1.5, 7.5, 47, 388.5, 261, 43, 24]
+console.log("Squareroot of Array:", result);
