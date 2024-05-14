3bd2ee777e4b86f7119cbdbb8d68267ca03eacee
kikic777 <166079146+kikic777@users.noreply.github.com>
2024-05-14 16:29:48 -0700

    Added console.log

--- experiments/Button/lab.js
+++ experiments/Button/lab.js
@@ -1,10 +1,11 @@
+console.log('Hello from index.js');
 
 function getNameAndDisplay() {
     // prompt the user to input their name with 
     let name = prompt("Name please:"); 
     // Use this input to change the text of the h1 using 
-    $('#title').html("Hello, " + name);
+    $('#title').html('Hello, ' + name);
 }
 
 // Attach a click handler to the button within the JS file
-// $("#my-button").click()
+// $("#my-button").click(getNameAndDisplay)
