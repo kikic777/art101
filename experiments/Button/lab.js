9967eef8ee6ec432a95e2066f41ea08068ce0e1a
kikic777 <166079146+kikic777@users.noreply.github.com>
2024-05-14 16:24:29 -0700

    Created header, button, made click element, and prompt window

---
+++ experiments/Button/lab.js
@@ -0,0 +1,10 @@
+
+function getNameAndDisplay() {
+    // prompt the user to input their name with 
+    let name = prompt("Name please:"); 
+    // Use this input to change the text of the h1 using 
+    $('#title').html("Hello, " + name);
+}
+
+// Attach a click handler to the button within the JS file
+// $("#my-button").click()
