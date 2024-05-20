dee1f2f4f5800da208cd40524616133c6f46ebb4
kikic777 <166079146+kikic777@users.noreply.github.com>
2024-05-20 15:20:35 -0700

    Edited chat box

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -5,8 +5,8 @@
 
 function generateRandomText() {
     const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-    const min = 20;
-    const max = 100;
+    const min = 50;
+    const max = 200;
     const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
     // Get a random starting index to slice the Lorem Ipsum text
     const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
@@ -44,8 +44,7 @@ function generateRandomText() {
   
      // Append a new div to our output div, with the appropriate class added
      $("#output").append('<div class="text ' + classToAdd + '"><p>' + newText + '</p></div>');
-  });
-
+ });
 
 
     // Append a new div to our output div, with the appropriate classes added
