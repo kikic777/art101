ced4d9807d154544b7a22da710f092a2c8f99f93
kikic777 <166079146+kikic777@users.noreply.github.com>
2024-05-20 01:17:22 -0700

    Added code to create fake dialog buttons

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,3 +1,28 @@
-//index.js - Working with a partner, 
+//index.js - Working with your partner, experiment with DOM manipulation. This simple JavaScript/jQuery script appends new elements to an output div\
+//Requirements: jQuery must be loaded for this script to work.
 //Author: Cynthia Ramirez
 //Date: 5//2024
+
+// This function will generate random lorem ipsum text fir our fake dialog
+function generateRandomText() {
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Get a random starting index to slice the Lorem Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+  }
+  
+  // click listener for button
+  $("#make-convo").click(function(){ 
+     // console.log("the button is clicked");
+      
+     // get new fake dialogue
+      const newText = generateRandomText();
+     // console.log("newText: " + newText);
+  
+    // append a new div to our output div
+      $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+    });
