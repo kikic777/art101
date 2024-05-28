5e1e7c0a30de50e85a6abfdc6dabf4876f7981df
kikic777 <166079146+kikic777@users.noreply.github.com>
2024-05-27 20:47:27 -0700

    Added input

---
+++ lab11/js/lab.js
@@ -0,0 +1,4 @@
+//index.js - Working with your partner, experiment with JS events. This simple JavaScript/jQuery script gets a value from an input field and outputs a modified version.
+//Requirements: jQuery must be loaded for this script to work.
+//Author: Cynthia Ramirez
+//Date: 5/20/2024
