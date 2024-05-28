f36716e3f468fcc07efedc0e74310cf02025e5a3
kikic777 <166079146+kikic777@users.noreply.github.com>
2024-05-27 21:08:21 -0700

    Created input and button click

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -2,3 +2,22 @@
 //Requirements: jQuery must be loaded for this script to work.
 //Author: Cynthia Ramirez
 //Date: 5/20/2024
+
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+    // We have to convert our string to an array and back again to sort it
+    return inputString.split('').sort().join('');
+}
+
+// click listener for button
+$("#submit").click(function(){
+    // get value of input field
+    const userName = $("#user-name").val();
+    
+  
+    // now let's sort it and append it to the output div
+    userNameSorted = sortString(userName);
+    
+    // append a new div to our output div
+   $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
