7fa73045ef7d0312560eca3a30de554b36067a0c
kikic777 <166079146+kikic777@users.noreply.github.com>
2024-05-28 01:18:43 -0700

    Tried Task X
    Created buttons that changed parts of the page, attempted to create a glitch toggled by a button (unsuccessful) and a timer (also unsuccessful,) created a button that creates more buttons instead

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -36,3 +36,74 @@ $("#submit").click(function(){
 
     $("#output").html('<div class="text"><p>' + "Sorted Name: " + userNameSorted + "<br>" + "Anagram: " + userNameRandomized + '</p></div>');
 });
+
+// add button to challenge section
+$(".minor-section").append("<button id='buttonChallenge'>Make Special</button>");
+
+// add a click listener to the challenge button
+$("#buttonChallenge").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special");
+     console.log("Challenge button is clicked!")
+});
+
+// add button to problems section
+// $(".minor-section").append("<button id='buttonProblems'>Make Special</button>");
+$(".minor-section").append("<div id='centeredButton'><button id='buttonProblems'>Make Special</button></div>");
+
+// add a click listener to the problems button
+$("#buttonProblems").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#problems").toggleClass("special");
+     console.log("Problems button is clicked!")
+});
+
+
+$(".minor-section").append("<button id='buttonGlitch'>Make Special</button>");
+   $("#buttonGlitch").click(function(){
+        $(".minor-section").append("<button id='glitch'>Make Special</button>");
+   });
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Select all the elements with the "glitchit" class
+  let glitchElements = document.querySelectorAll('.glitchit');
+
+  // Loop through each element and apply the glitch effect
+  glitchElements.forEach(function(element) {
+    // Add a random amount of jitter to each element's position
+    let jitterX = Math.floor(Math.random() * 3) - 1;
+    let jitterY = Math.floor(Math.random() * 3) - 1;
+
+     // Apply the animation to each element with added position jitters
+     element.style.animation = 'glitchit .1s infinite alternate-reverse';
+     element.style.transform = 'translate(' + jitterX + 'px, ' + jitterY + 'px)';
+   });
+
+   $("#buttonGlitch").click(function(){
+        $(".minor-section .glitchit").toggleClass("makeSpecial");
+     console.log("Glitch button is clicked!")
+   });
+});
+
+
+// Trigger the glitch after 0.5 seconds
+window.onload = function() {
+setTimeout(() => {
+  console.log("Set timeout")
+  // Select all the elements with the "glitchit" class
+  let glitchElements = document.querySelectorAll('.glitchit');
+
+  // Loop through each element and apply the glitch effect
+  glitchElements.forEach(function(element) {
+    // Add a random amount of jitter to each element's position
+    let jitterX = Math.floor(Math.random() * 3) - 1;
+    let jitterY = Math.floor(Math.random() * 3) - 1;
+
+     // Apply the animation to each element with added position jitters
+     element.style.animation = 'glitchit .1s infinite alternate-reverse';
+     element.style.transform = 'translate(' + jitterX + 'px, ' + jitterY + 'px)';
+   });
+  }, 5000);
+}
+
+
